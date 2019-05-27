97d0e899aa995311ac7ce9232beb42862108dead
ruthy <571341639@qq.com>
2019-05-27 16:15:56 +0800

    line_profiler

---
+++ 5.性能分析工具/line_profiler_test.py
@@ -0,0 +1,14 @@
+import time
+
+@profile
+def fun():
+    a = 0
+    b = 0
+    for i in range(100000):
+        a = a + i * i
+    for i in range(3):
+        b += 1
+        time.sleep(0.1)
+    return a + b
+if __name__=='__main__':
+    fun()
